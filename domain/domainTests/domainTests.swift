abb4a70ddafd1e258fcb9d31784c256a9c1acd68
Nazim Asadov <nazimatmu@gmail.com>
2022-08-21 11:07:27 +0400

    Changes added

--- domain/domainTests/domainTests.swift
+++ domain/domainTests/domainTests.swift
@@ -10,12 +10,8 @@ import XCTest
 class domainTests: XCTestCase {
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-
 }
