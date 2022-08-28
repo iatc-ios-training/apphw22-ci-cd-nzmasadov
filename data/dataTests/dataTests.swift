94e98609473ef03e5e764d4d8c4f33b31afdb009
Nazim Asadov <nazimatmu@gmail.com>
2022-08-29 00:28:14 +0400

    last commit i hope

--- data/dataTests/dataTests.swift
+++ data/dataTests/dataTests.swift
@@ -12,7 +12,6 @@ class dataTests: XCTestCase {
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
