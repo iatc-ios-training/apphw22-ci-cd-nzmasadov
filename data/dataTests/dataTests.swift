b80415c5611236aef031451de525165b10e555e2
Nazim Asadov <nazimatmu@gmail.com>
2022-08-29 00:13:16 +0400

    Last commit

--- data/dataTests/dataTests.swift
+++ data/dataTests/dataTests.swift
@@ -16,5 +16,4 @@ class dataTests: XCTestCase {
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
 }
